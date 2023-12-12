bc53e90a78b0489401fa9f79187f7ac719747059
Liplan Lekipising <leeplanke@gmail.com>
2023-12-12 19:01:18 +0300

    add global head

---
+++ utils/head.tsx
@@ -0,0 +1,29 @@
+
+// a reusable component for the <head> section of each page. 
+import Head from "next/head";
+import React from "react";
+
+export default function Header({
+  title = "My Awesome App: short message!",
+  description = "Your app description",
+  image = "The URL of the image to be used when sharing on social media",
+}) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="title" content={title} />
+      <meta property="og:title" content={title} />
+      <meta name="twitter:title" content={title} />
+      <meta name="description" content={description} />
+      <meta property="og:description" content={description} />
+      <meta name="twitter:description" content={description} />
+      <meta property="image" content={image} />
+      <meta property="og:image" content={image} />
+      <meta name="twitter:image" content={image} />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1 maximum-scale=1 user-scalable=no"
+      />
+    </Head>
+  );
+}
